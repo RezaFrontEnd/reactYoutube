10ac6f499af2e16179250fc67914eda5c98b7a66
reza ahmadi <reza.ahmadi350@gmail.com>
2026-08-08 04:19:48 +0330

    recipeApp(train)

--- src/components/recipeApp/FoodItem.jsx
+++ src/components/recipeApp/FoodItem.jsx
@@ -1,13 +1,12 @@
 
 export default function FoodItems({food,setFoodId}) {
   return (
-    <div className="w-[300px] rounded-[8px] shadow-[0_4px_6px_rgba(0,0,0,.5)] m-[20px] overflow-hidden
-    font-[Polysans,sans-serif]">
-        <div className="font-[Arial,Helvetica,sans-serif] text-center">
-            <p className="font-[1em] font-semibold ">{food.strMeal}</p>
+    <div className="foodItemContainer">
+        <div className="foodItemDiv2">
+            <p className="foodItemsTitle">{food.strMeal}</p>
         </div>
         <img src={food.strMealThumb} className="max-w-[100%] h-auto color-[#393e46]"  />
-       <div className="mb-[15px] flex justify-around text-center mt-3">
+       <div className="foodItemsDiv3">
            <button className=" bg-[#29bb89] text-white p-[10px] text-[14px] font-semibold
               cursor-pointer rounded-[10px]" onClick={()=>setFoodId(food.idMeal)}>view Recipe</button>
           </div>
--- src/components/recipeApp/Nav.jsx
+++ src/components/recipeApp/Nav.jsx
@@ -1,8 +1,7 @@
 
 export default function Nav(){
   return (
-    <div className="text-[2em] font-[Arial,Helvetica,sans-serif] font-bold text-[#393e46] 
-    p-[30px] shadow-[0px_0px_10px_rgba(0,0,0,.5)]">
+    <div className="nav">
         🍔 FoodApp  
     </div>
   );
